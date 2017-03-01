06ae03a48c12b764eb31f440267e793583aadd19
Tim <timotheeaneumann@gmail.com>
2017-03-01 12:11:24 -0800

    dnd to empty lanes

--- app/stores/LaneStore.js
+++ app/stores/LaneStore.js
@@ -1,6 +1,7 @@
 import uuid from 'node-uuid';
 import alt from '../libs/alt';
 import LaneActions from '../actions/LaneActions';
+import update from 'react-addons-update'
 
 class LaneStore {
 	constructor() {
@@ -40,6 +41,9 @@ class LaneStore {
 
 	attachToLane({laneId, noteId}) {
 		const lanes = this.lanes.map(lane => {
+			if(lane.notes.includes(noteId)) {
+				lane.notes = lane.notes.filter(note => note !== noteId);
+			}
 			if(lane.id === laneId) {
 				if(lane.notes.includes(noteId)) {
 					console.warn('Already attached note to lane', lanes);
@@ -63,6 +67,33 @@ class LaneStore {
 
 			return lane;
 		});
+
+		this.setState({lanes});
+	}
+
+	move({sourceId, targetId}) {
+		const lanes = this.lanes;
+		const sourceLane = lanes.filter(lane => lane.notes.includes(sourceId))[0];
+		const targetLane = lanes.filter(lane => lane.notes.includes(targetId))[0];
+		const sourceNoteIndex = sourceLane.notes.indexOf(sourceId);
+		const targetNoteIndex = targetLane.notes.indexOf(targetId);
+
+		if(sourceLane === targetLane) {
+			// Move at once to avoid complications
+			sourceLane.notes = update(sourceLane.notes, {
+				$splice: [
+					[sourceNoteIndex, 1],
+					[targetNoteIndex, 0, sourceId]
+				]
+			});
+		}
+		else {
+			// get rid of the source
+			sourceLane.notes.splice(sourceNoteIndex, 1);
+
+			// and move it to target
+			targetLane.notes.splice(targetNoteIndex, 0 , sourceId);
+		}
 
 		this.setState({lanes});
 	}
